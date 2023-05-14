192adac71ef845a67b0ad7e9c54fc03a35b29de1
marshka <ivan.marisca@hotmail.it>
2023-05-14 23:41:18 +0200

    Updated schema and bug fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,51 +44,6 @@ $("a.smooth-scroll").click(function(event) {
   }
 });
 
-// Photo Filter
-var activeFilter = "all";
-
-$(".ww-filter-button").on("click", function(e) {
-  // remove btn-primary from all buttons first
-  $(".ww-filter-button").removeClass("btn-primary");
-  $(".ww-filter-button").addClass("btn-outline-primary");
-
-  // add btn-primary to active button
-  var button = $(this);
-  button.removeClass("btn-outline-primary");
-  button.addClass("btn-primary");
-  filterItems(button.data("filter"));
-  e.preventDefault();
-});
-
-function filterItems(filter) {
-  if (filter === activeFilter) {
-    return;
-  }
-
-  activeFilter = filter;
-  $(".ww-gallery .card").each(function() {
-    var card = $(this);
-    var groups = card.data("groups");
-    var show = false;
-    if (filter === "all") {
-      show = true;
-    } else {
-      for (var i = 0; i < groups.length; i++) {
-        if (groups[i] === filter) {
-          show = true;
-        }
-      }
-    }
-    // hide everything first
-    card.fadeOut(400);
-    setTimeout(function() {
-      if (show && !card.is(":visible")) {
-        card.fadeIn(400);
-      }
-    }, 500);
-  });
-}
-
 // Light Box
 $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
